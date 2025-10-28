342ebc70240fb981d97d684e1ec31eceefea8038
FerAnana <f.s.anana.g@gmail.com>
2025-10-27 22:35:35 -0300

    Excercise 3 completed

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int magicInt = randomGenerator.Next(1, 100);
+        int guessInt;
+
+        do
+        {
+            Console.Write("What is your guess? ");
+            string guessString = Console.ReadLine();
+            guessInt = int.Parse(guessString);
+            if (guessInt == magicInt)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+            else if (guessInt > magicInt)
+            {
+                Console.WriteLine("Lower.");
+            }
+            else if (guessInt < magicInt)
+            {
+                Console.WriteLine("Higher.");
+            }
+        } while (guessInt != magicInt);
     }
 }
