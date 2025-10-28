6477948332cb89ee7910b1324e2065c7e70cf0b8
FerAnana <f.s.anana.g@gmail.com>
2025-10-27 21:06:39 -0300

    Excercises 1 and 2 completes

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage?");
+        string gradeString = Console.ReadLine();
+        int gradeInt = int.Parse(gradeString);
+        string letter = "";
+        if (gradeInt >= 70)
+            if (gradeInt >= 90)
+            {
+                letter = "A";
+            }
+            else if (gradeInt >= 80)
+            {
+                letter = "B";
+            }
+            else if (gradeInt >= 70)
+            {
+                letter = "C";
+            }
+        if (gradeInt < 70)
+            if (gradeInt >= 60)
+            {
+                letter = "D";
+            }
+            else if (gradeInt < 60)
+            {
+                letter = "F";
+            }
+        if (gradeInt >= 70 && letter == "A")
+        {
+            Console.WriteLine($"Congratulation! You passed the coruse with an {letter}!");
+        }
+        else if (gradeInt >= 70 && letter != "A")
+        {
+            Console.WriteLine($"Congratulation! You passed the coruse with a {letter}!");
+        }
+        else if (gradeInt < 70)
+        {
+            Console.WriteLine($"Your grade is a {letter}. The next semester will be better!");
+        }
     }
 }
