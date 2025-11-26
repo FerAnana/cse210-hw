3cbedd5ae4d5d8ccdfee0dd3b38321c835757a22
FerAnana <f.s.anana.g@gmail.com>
2025-11-26 16:47:10 -0300

    Homework assignment done

---
+++ week05/Homework/Assignment.cs
@@ -0,0 +1,23 @@
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
+
+public class Assignment
+{
+  private string _studentName;
+  private string _topic;
+
+public string GetStudentName()
+  {
+    return _studentName;  
+  }
+  public Assignment(string studentName, string topic)
+  {
+    _studentName = studentName;
+    _topic = topic;
+  }
+
+  public string GetSummary()
+  {
+    return $"{_studentName} - {_topic}";
+  }
+}
--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment student = new Assignment("Fernando Añaña", "Programming");
+        MathAssignment studentMath = new MathAssignment("Sofia Nievas", "Fractions", "7.3", "8-19");
+        WritingAssignment studentWriting = new WritingAssignment("Rodrigo Luna", "European History", "The Causes of World War II");
+
+        Console.WriteLine(student.GetSummary());
+        Console.WriteLine($"\n{studentMath.GetHomeworkList()}");
+        Console.WriteLine($"\n{studentWriting.GetWritingAssignment()}");
     }
 }
