e33732a85111fc5d47a7695457ae7923a4e3a872
FerAnana <f.s.anana.g@gmail.com>
2025-12-03 19:13:31 -0300

    Assignment done

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Drawing;
+using System.Formats.Asn1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        Square shape1 = new Square("blue", 6);
+        Rectangle shape2 = new Rectangle("orange", 4, 6);
+        Circle shape3 = new Circle("skyblue", 5);
+
+        shapes.Add(shape1);
+        shapes.Add(shape2);
+        shapes.Add(shape3);
+
+        foreach (Shape shape in shapes)
+        {
+            double area = shape.GetArea();
+            string color = shape.GetColor();
+            Console.WriteLine(area);
+            Console.WriteLine(color);
+        }
     }
 }
